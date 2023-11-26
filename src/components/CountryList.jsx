bd232b206c092cbb5f86a5a2427eaef93c78f696
Josh Wang <Joshw9012@gmail.com>
2023-11-27 10:42:33 +1100

    237 - finished

---
+++ src/components/CountryList.jsx
@@ -0,0 +1,31 @@
+import CountryItem from "./CountryItem";
+import Spinner from "./Spinner";
+import styles from "./CountryList.module.css";
+import Message from "./Message";
+import { useCities } from "../contexts/CitiesContext";
+
+function CountryList() {
+  const { cities, isLoading } = useCities();
+
+  if (isLoading) return <Spinner> </Spinner>;
+  //console.log(cities);
+  if (!cities.length)
+    return <Message message="Add your first city by clicking on the map" />;
+
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    else return arr;
+  }, []);
+
+  // console.log(countries);
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.country} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
